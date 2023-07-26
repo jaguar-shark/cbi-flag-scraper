3ee5a4112265bd19eedb898057665fd0bc1f1afa
Dalton <daltonryandesigns@gmail.com>
2023-07-26 12:00:38 -0400

    added while statement to repeated check the API for color updates

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from bs4 import BeautifulSoup
 # API request to community boating to get flag color
 cbi_URL = "https://api.community-boating.org/api/flag"
 
-
 # Function for calling CBI api and returning the flag color
 def flagCheck(tries, url):
+    print("Checking flag status for CBI...")
     for i in range(tries):
         try: 
-            # Receive content of URL
+            # Receive content of URL and store color character
+            # cbi_URL example content: var FLAG_COLOR = "G"
             page = requests.get(url)
             string = str(BeautifulSoup(page.content, "html.parser"))
             flagColor = string[18]
+            # Check which color code the API provides and assign it a full color word string
             if flagColor == "Y":
                     return "Yellow"
             elif flagColor == "R":
@@ -29,6 +31,14 @@ def flagCheck(tries, url):
                 continue
             raise
         break
-    return flagColor
+    return flagColor 
 
-print(flagCheck(5,cbi_URL))
+
+def main():
+    print("Intializing CBI Flag Scrapper:")
+    while True:
+        print("Flag color is: " + flagCheck(5,cbi_URL))
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
