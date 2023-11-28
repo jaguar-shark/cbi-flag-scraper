aa1d44ab276a96fc2d18887788ab3a13aff7b31a
Dalton <daltonryandesigns@gmail.com>
2023-11-28 17:36:58 -0500

    Removed tries logic and cleaned up some printed messages

--- main.py
+++ main.py
@@ -7,37 +7,33 @@ cbi_URL = "https://api.community-boating.org/api/flag"
 
 
 # Function for calling Community Boating Inc. API and returning the flag color
-def flagCheck(tries, url):
-    for i in range(tries):
-        try:
-            # Receive content of URL and store color character
-            # cbi_URL example content: var FLAG_COLOR = "G"
-            page = requests.get(url)
-            string = str(BeautifulSoup(page.content, "html.parser"))
-            flagColor = string[18]
-            # Check which color code the API provides and assign it a full color word string
-            if flagColor == "Y":
-                return "Yellow"
-            elif flagColor == "R":
-                return "Red"
-            elif flagColor == "G":
-                return "Green"
-            else:
-                return "Conditions unknown"
-        except Exception as error:
-            print(error)
-            if i < tries - 1:
-                continue
-            raise
-        break
+def flagCheck(url):
+    try:
+        # Receive content of URL and store color character
+        # cbi_URL example content: var FLAG_COLOR = "G"
+        page = requests.get(url)
+        string = str(BeautifulSoup(page.content, "html.parser"))
+        flagColor = string[18]
+        # Check which color code the API provides and assign it a full color word string
+        if flagColor == "Y":
+            return "Yellow"
+        elif flagColor == "R":
+            return "Red"
+        elif flagColor == "G":
+            return "Green"
+        else:
+            return "Conditions unknown"
+    except Exception as error:
+        print(error)
     return flagColor
 
 
 def main():
-    print("\nIntializing CBI Flag Check: \n")
+    print("\n\nStarting up Community Boating Inc. Flag Check... \n\n")
+    time.sleep(2)
     while True:
         print("Checking flag status for CBI...")
-        print("Flag color is: " + flagCheck(5, cbi_URL))
+        print("Flag color is: " + flagCheck(cbi_URL))
         time.sleep(5)
 
 
